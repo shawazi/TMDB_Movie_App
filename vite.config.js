c796ff93d4900cb5fc19dc87a82f01a22d3e7f04
“shawazi” <shawazisonfire@gmail.com>
2023-07-01 20:53:56 -0400

    adjustments to image paths, added favicon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// TO CONNECT TO GH PAGES DIRECTLY: base: '/TMDB_Movie_App/', 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
 })
+
