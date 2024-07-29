581831f5c4be6d8af8fe58962bf1ff4d332ad522
shawazi <shawazisonfire@gmail.com>
2024-07-29 02:38:50 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // TO CONNECT TO GH PAGES DIRECTLY: base: '/TMDB_Movie_App/', 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/<REPO>/',
   plugins: [react()],
 })
 
